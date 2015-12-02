0569e7299a58b22ed21f679fb9ccb11048060c4a
LosPibesBienMachos <sebastianariell@hotmail.com.ar>
2015-12-02 16:41:40 -0300

    dale que so vo

--- Duelo-Android/app/src/main/java/uis/tp/grupo1/duelodeleyendas/Services/RepoPersonajes.java
+++ Duelo-Android/app/src/main/java/uis/tp/grupo1/duelodeleyendas/Services/RepoPersonajes.java
@@ -1,7 +1,11 @@
 package uis.tp.grupo1.duelodeleyendas.Services;
 
 
+import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import uis.tp.grupo1.duelodeleyendas.Model.PersonajeRep;
 
 /**
  * Created by sebastian on 26/10/15.
@@ -16,12 +20,13 @@ public class RepoPersonajes {
 
 
     public static PersonajesServices createPersonajesServices(){
-        String SERVER_IP = "127.0.0.1";
+        String SERVER_IP = "10.12.0.200";
         String SERVER_IP_GENY= "127.0.0.1";
-        String API_URL = "http://192.168.1.37:9000";
+        String API_URL = "http://10.12.0.200:9000";
         RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(API_URL).build();
         PersonajesServices personajesServicess = restAdapter.create(PersonajesServices.class);
         return personajesServicess;
     }
+
 
 }
